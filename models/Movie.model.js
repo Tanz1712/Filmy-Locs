b46b12670b6c41f7d2f5e5a49e1dc3af50b02f4c
zarahsara <sarahabbasi25@aol.com>
2022-07-01 20:37:40 +0100

    testing

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,15 +1,15 @@
 const { Schema, model } = require("mongoose");
 
 const movieSchema = new Schema({
-  name: { type: String },
-  description: { type: String },
-  imageUrl: { 
-    type: String, 
-    required: [true, "imageUrl is required."], 
-},
-  owner: { type: Schema.Types.ObjectId, ref: "User" },
-  // reviews: [] // we will update this field a bit later when we create review model
-  // reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }];
-});
+    title: String,
+    Director: String,
+    cast: [String],
+    image: String,
+    loctions: [{ type: Schema.Types.ObjectId, ref: "Location" }],
+    country: String,
+   
+  });
 
-module.exports = model("Movie", movieSchema);
+const Movie = model("Movie", movieSchema)
+
+module.exports = Movie;
