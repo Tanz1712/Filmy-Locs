2a3602abdf14a4fadcf426ca27cb05f401942a83
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-07-16 12:03:23 +0200

    done

--- bin/locations.seeds.js
+++ bin/locations.seeds.js
@@ -2,7 +2,24 @@ const mongoose = require("mongoose");
 const Movie = require("../models/Movie.model");
 const Location = require("../models/Location.model");
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/project-2";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://ironhacker:tan@tan.sinaeps.mongodb.net/?retryWrites=true&w=majority";
+
+const locations = [
+  {
+    name: ["United Kindom", "Manchester"],
+    description: "London has been descriped as a world cultural capital",
+  },
+  {
+    name: ["united States"], 
+    description:
+      "The USA packs spectacular biodiversity within its borders, boasting some of the most beautiful places in America",
+  },
+  {
+    name: ["France", "Paris"],
+    description:
+      "30 million foreign visitors, and so is one of the most visited cities in the world. Paris sights to see the Eiffel Tower",
+  },
+];
 
 mongoose
   .connect(MONGO_URI)
@@ -10,29 +27,7 @@ mongoose
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
-
-const locations = [
-  {
-    name: "United Kindom",
-    description: "London has been descriped as a world cultural capital",
-  },
-  {
-    name: "united State",
-    description:
-      "The USA packs spectacular biodiversity within its borders, boasting some of the most beautiful places in America",
-  },
-  {
-    name: "France",
-    description:
-      "30 million foreign visitors, and so is one of the most visited cities in the world. Paris sights to see the Eiffel Tower",
-  },
-];
-
-Location.create(locations)
+    Location.create(locations)
   .then((locationsFromDB) => {
     console.log(`Created ${locationsFromDB.length} locations`);
 
@@ -42,3 +37,9 @@ Location.create(locations)
   .catch((err) =>
     console.log(`An error occurred while creating location from the DB: ${err}`)
   );
+
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
--- bin/movies.seeds.js
+++ bin/movies.seeds.js
@@ -2,35 +2,28 @@ const mongoose = require("mongoose");
 const Movie = require("../models/Movie.model");
 const Location = require("../models/Location.model");
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/project-2";
+const MONGO_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://ironhacker:tan@tan.sinaeps.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
 
 const movies = [
   {
     title: "Skyfall",
     imageUrl:
-      "https://www.amazon.com/James-Skyfall-October-Teaser-Poster/dp/B00CID6LX2",
+      "https://res.cloudinary.com/der1ghtaj/image/upload/v1657963143/project-2-images/efjzwwlrxglkqnta7znf.jpg",
     director: "Sam Mendes",
     cast: ["Daniel Craig", "Javier Bardem", "Judi Dench"],
     plot: "When James Bond's (Daniel Craig) latest assignment goes terribly wrong, it leads to a calamitous turn of events: Undercover agents around the world are exposed, and MI6 is attacked, forcing M (Judi Dench) to relocate the agency. ",
 
     relasedate: 23 / 10 / 2012,
     country: "United Kindom",
+
   },
   {
     title: "The Fugitive",
     imageUrl:
-      "https://www.mauvais-genres.com/en/us-movie-posters/20458-the-fugitive-original-movie-poster-27x40-in-1993-andrew-davis-harrison-ford-3701092836559.html",
+      "https://res.cloudinary.com/der1ghtaj/image/upload/v1657963376/project-2-images/jtpxy1xa4exfmrehyfhy.jpg",
     director: "Andrew Davis",
     cast: ["Tommy Lee Jones", "Sela Ward", "Harrison Ford"],
     plot: "Dr Richard Kimble is falsely charged with killing his wife. When Deputy Marshal Samuel Gerard pursues him after he escapes from prison, Richard realises that he cannot trust anybody.",
@@ -40,22 +33,33 @@ const movies = [
   {
     title: "French Kiss",
     imageUrl:
-      "url=https://www.amazon.com/French-Kiss-Meg-Ryan/dp/B000035Z1Z/ref=sr_1_1?crid=1XM8Q8K9X9NYT&keywords=french+kiss+%281995+cast%29&qid=1657570674&sprefix=%2Caps%2C2017&sr=8-1",
+      "https://res.cloudinary.com/der1ghtaj/image/upload/v1657963269/project-2-images/mdmk3ou0wqxtypsuse9z.jpg",
     director: "Lawrence kasdan",
     cast: ["Meg Ryan", "Kevin Kline", "Jean Reno"],
     plot: "Kate plans to confront her straying fiancé who stays in France. On her way she meets Luc, a thief, who hides a stolen necklace in her bag. Her life changes when she falls in love with Luc.",
     relasedate: 5 / 5 / 1995,
-    country: "France",   
+    country: "France",
   },
 ];
 
-Movie.create(movies)
-  .then((allTheMoviesFromDB) => {
-    console.log(`Created ${allTheMoviesFromDB.length} movies`);
-  //  .then(() => res.render("/moviesdemo"))
-    // Once created, close the DB connection
-    mongoose.connection.close();
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+    Movie.create(movies)
+      .then((allTheMoviesFromDB) => {
+        console.log(`Created ${allTheMoviesFromDB.length} movies`);
+        // Once created, close the DB connection
+        mongoose.connection.close();
+      })
+      .catch((err) =>
+        console.log(
+          `An error occurred while creating movies from the DB: ${err}`
+        )
+      );
   })
-  .catch((err) =>
-    console.log(`An error occurred while creating movies from the DB: ${err}`)
-  );
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
